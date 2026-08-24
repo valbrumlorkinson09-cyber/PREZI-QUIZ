14da3198910fe48503dd0fea158846c19d27c1c2
PreziScore <valbrumlorkinson09@gmail.com>
2026-08-23 22:53:49 -0400

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -4,36 +4,12 @@
 
 const ALL_QUESTIONS = [
 
+    ...(typeof SPORTS_QUESTIONS !== "undefined"
+        ? SPORTS_QUESTIONS
+        : []),
+
     ...(typeof FOOTBALL_QUESTIONS !== "undefined"
         ? FOOTBALL_QUESTIONS
-        : []),
-
-    ...(typeof SCIENCE_QUESTIONS !== "undefined"
-        ? SCIENCE_QUESTIONS
-        : []),
-
-    ...(typeof HISTORY_QUESTIONS !== "undefined"
-        ? HISTORY_QUESTIONS
-        : []),
-
-    ...(typeof GEOGRAPHY_QUESTIONS !== "undefined"
-        ? GEOGRAPHY_QUESTIONS
-        : []),
-
-    ...(typeof CINEMA_QUESTIONS !== "undefined"
-        ? CINEMA_QUESTIONS
-        : []),
-
-    ...(typeof MUSIC_QUESTIONS !== "undefined"
-        ? MUSIC_QUESTIONS
-        : []),
-
-    ...(typeof TECHNOLOGY_QUESTIONS !== "undefined"
-        ? TECHNOLOGY_QUESTIONS
-        : []),
-
-    ...(typeof CULTURE_QUESTIONS !== "undefined"
-        ? CULTURE_QUESTIONS
         : [])
 
 ];
